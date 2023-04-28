a5d4b523518a3a1a24ac48e5bfb9485944f3fc5f
SinghHridyansh <Hridyansh.360@gmail.com>
2023-04-28 21:41:43 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const element1 = React.createElement(
+  "h1",
+  { className: "heading" },
+  "Learn web development"
+);
+const element2 = React.createElement(
+  "p",
+  { className: "para" },
+  "Welcome to the MDN learning area. This set of articles aims to guide complete beginners to web development with all that they need to start coding websites."
+);
+const element3 = React.createElement(
+  "p",
+  { className: "para" },
+  "The aim of this area of MDN is not to take you from beginner to expert but to take you from beginner to comfortable. From there, you should be able to start making your way, learning from the rest of MDN and other intermediate to advanced resources that assume a lot of previous knowledge."
+);
+const element4 = React.createElement(
+  "p",
+  { className: "para" },
+  "If you are a complete beginner, web development can be challenging — we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work."
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <div className="app">
+    {element1}
+    {element2}
+    {element3}
+    {element4}
+  </div>,
+  document.getElementById("root")
+);
